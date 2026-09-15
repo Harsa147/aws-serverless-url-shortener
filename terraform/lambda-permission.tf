1381246a9106f32c421f0442b272d1ce2d97c90a
devil <harsas147@gmail.com>
2026-09-15 22:21:18 +0530

    style: format Terraform configuration

--- terraform/lambda-permission.tf
+++ terraform/lambda-permission.tf
@@ -3,7 +3,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 resource "aws_lambda_permission" "api_gateway_post_1" {
-  statement_id  = "lambda-626ebaae-51a7-4796-a822-1bcbe28536d3"
+  statement_id = "lambda-626ebaae-51a7-4796-a822-1bcbe28536d3"
 
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.url_shortener.function_name
@@ -13,7 +13,7 @@ resource "aws_lambda_permission" "api_gateway_post_1" {
 }
 
 resource "aws_lambda_permission" "api_gateway_get" {
-  statement_id  = "ad2d5a2e-8957-595a-b6de-c2fb0256cb79"
+  statement_id = "ad2d5a2e-8957-595a-b6de-c2fb0256cb79"
 
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.url_shortener.function_name
